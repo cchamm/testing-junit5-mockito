3546b2e90d1e3fadb3dc4069de4a520e95d99ba7
Cham <runningpaipai@gmail.com>
2024-02-17 11:57:22 +0800

    Feat: Different way to create mockito object

---
+++ src/test/java/guru/springframework/AnnotationMocksTest.java
@@ -0,0 +1,23 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Map;
+
+public class AnnotationMocksTest {
+@BeforeEach
+void setUp() {
+    MockitoAnnotations.initMocks(this);
+}
+    @Mock
+    Map<String, Object> mapMock;
+
+
+    @Test
+    void testMock() {
+        mapMock.put("keyvalue", "foo");
+    }
+}
